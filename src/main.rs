fab05e2f50feca27616121b9198a67eae70fede3
Deepak <deepak@tarkalabs.com>
2022-05-10 16:34:26 -0300

    Add anyhow error handling skeleton.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
+use std::fmt;
+use std::fmt::Display;
+use std::io::Error;
+use std::time::Duration;
+
+use axum::body::Body;
 use axum::body::boxed;
 use axum::body::Empty;
 use axum::body::Full;
 use axum::extract::{Path, Form};
-use axum::http::HeaderValue;
+use axum::http::Response;
+use axum::http::{HeaderValue, StatusCode};
 use axum::response::IntoResponse;
 use axum::response::Redirect;
-use axum::response::Response;
 use axum::routing::{get, Router, post};
-use axum::Server;
+use axum::error_handling::HandleError;
+use axum::{Server, BoxError};
+use axum::error_handling::HandleErrorLayer;
 use include_dir::{include_dir, Dir};
 use serde::{Serialize, Deserialize};
+use tower::{self, ServiceBuilder};
+use std::env;
+
+use anyhow::{Result, Context};
 
 static STATIC_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/static");
 static BOOK_DIR: Dir<'_> = include_dir!("$OUT_DIR");
@@ -54,9 +66,30 @@ struct Invite {
     email: String
 }
 
-async fn join(Form(invite): Form<Invite>) -> impl IntoResponse {
+#[derive(Debug)]
+struct MyError {
+  msg: String
+}
+impl Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({})", self.msg)
+    }
+}
+async fn join(Form(invite): Form<Invite>) -> Result<String> {
     println!("The invite is {:?}", invite);
-    "dope!"
+    // env::var_os("Something").ok_or(Err)
+    Err(anyhow::anyhow!("Anyhow error"))
+}
+
+
+
+async fn handle_anyhow_errors(err: anyhow::Error) -> (StatusCode, String) {
+    {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Unhandled internal error: {}", err),
+        )
+    }
 }
 
 #[tokio::main]
@@ -65,7 +98,14 @@ async fn main() {
         .route("/", get(|| async { Redirect::to("/book/index.html")}))
         .route("/static/*path", get(serve_file))
         .route("/book/*path", get(serve_book))
-        .route("/join", post(join));
+        // .route("/join", post(join))
+        .route("/join", HandleError::new(join, handle_anyhow_errors));
+        // .layer(
+        //     ServiceBuilder::new()
+        //         .layer(HandleErrorLayer::new(handle_anyhow_errors))
+        //         .timeout(Duration::from_secs(10))
+        //         .into_inner()
+        // );
 
     Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(router.into_make_service())
